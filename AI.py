49d866f900efbbfa401faced8a2949043df91604
Tru-Apicella <105618454+Tru-Apicella@users.noreply.github.com>
2022-10-29 14:39:16 -0400

    Create AI.py

---
+++ AI.py
@@ -0,0 +1,121 @@
+'''
+a couple of assumptions for other parts not done yet:
+piece names - pawn bishop rook horse queen king
+valid move check - validMove()
+proccess: random move -> assign evaluation to the move -> put in binary tree -> continue for all moves ->
+eval n number of furture moves for all previous moves -> put in binary tree -> parse tree to find move with lowest/highest eval
+stuff done: piece bias and eval of each piece
+stuff needed: functuion to create random legal moves, function to create binary tree with eval nums,  
+function to search binary tree to find the highest/lowest eval total, and probably add alpha beta pruning
+'''
+wPawnBias = [
+        [0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
+        [5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0],
+        [1.0,  1.0,  2.0,  3.0,  3.0,  2.0,  1.0,  1.0],
+        [0.5,  0.5,  1.0,  2.5,  2.5,  1.0,  0.5,  0.5],
+        [0.0,  0.0,  0.0,  2.0,  2.0,  0.0,  0.0,  0.0],
+        [0.5, -0.5, -1.0,  0.0,  0.0, -1.0, -0.5,  0.5],
+        [0.5,  1.0, 1.0,  -2.0, -2.0,  1.0,  1.0,  0.5],
+        [0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0]
+    ]
+bPawnBias = wPawnBias[::-1]
+
+wRookBias = [
+    [  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
+    [  0.5,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [  0.0,   0.0, 0.0,  0.5,  0.5,  0.0,  0.0,  0.0]
+]
+bRookBias = wRookBias[::-1]
+
+wBishopBias = [
+    [ -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0],
+    [ -1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0],
+    [ -1.0,  0.0,  0.5,  1.0,  1.0,  0.5,  0.0, -1.0],
+    [ -1.0,  0.5,  0.5,  1.0,  1.0,  0.5,  0.5, -1.0],
+    [ -1.0,  0.0,  1.0,  1.0,  1.0,  1.0,  0.0, -1.0],
+    [ -1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0, -1.0],
+    [ -1.0,  0.5,  0.0,  0.0,  0.0,  0.0,  0.5, -1.0],
+    [ -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0]
+]
+bBishopBias = wBishopBias[::-1]
+
+horseBias = [
+        [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0],
+        [-4.0, -2.0,  0.0,  0.0,  0.0,  0.0, -2.0, -4.0],
+        [-3.0,  0.0,  1.0,  1.5,  1.5,  1.0,  0.0, -3.0],
+        [-3.0,  0.5,  1.5,  2.0,  2.0,  1.5,  0.5, -3.0],
+        [-3.0,  0.0,  1.5,  2.0,  2.0,  1.5,  0.0, -3.0],
+        [-3.0,  0.5,  1.0,  1.5,  1.5,  1.0,  0.5, -3.0],
+        [-4.0, -2.0,  0.0,  0.5,  0.5,  0.0, -2.0, -4.0],
+        [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0]
+    ]
+
+queenBias = [
+    [ -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0],
+    [ -1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0],
+    [ -1.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0],
+    [ -0.5,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5],
+    [  0.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5],
+    [ -1.0,  0.5,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0],
+    [ -1.0,  0.0,  0.5,  0.0,  0.0,  0.0,  0.0, -1.0],
+    [ -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0]
+]
+
+wKingBias = [
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -2.0, -3.0, -3.0, -4.0, -4.0, -3.0, -3.0, -2.0],
+    [ -1.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -1.0],
+    [  2.0,  2.0,  0.0,  0.0,  0.0,  0.0,  2.0,  2.0 ],
+    [  2.0,  3.0,  1.0,  0.0,  0.0,  1.0,  3.0,  2.0 ]
+]
+bKingBias = wKingBias[::-1]
+
+def pieceVal(piece,color,x,y):
+    if piece == 'pawn':
+        if color == 'w':
+            return 10 + wPawnBias[x][y]
+        elif color =='b':
+            return -10 - bPawnBias[x][y]
+    elif piece == 'rook':
+        if color == 'w':
+            return 50 + wRookBias[x][y]
+        elif color =='b':
+            return -50 - bRookBias[x][y]
+    elif piece == 'horse':
+        if color == 'w':
+            return 30 + horseBias[x][y]
+        elif color =='b':
+            return -30 - horseBias[x][y]
+    elif piece == 'bishop':
+        if color == 'w':
+            return 30 + wBishopBias[x][y]
+        elif color =='b':
+            return -30 - bBishopBias[x][y]
+    elif piece == 'queen':
+        if color == 'w':
+            return 90 + queenBias[x][y]
+        elif color =='b':
+            return -90 - queenBias[x][y]
+    if piece == 'king':
+        if color == 'w':
+            return 900 + wKingBias[x][y]
+        elif color =='b':
+            return -900 - bKingBias[x][y]
+
+def randomMove():
+    print ('placeholder')
+
+def createTree():
+    print('placeholder')
+
+def searchTree():
+    print('placeholder')
+print("teting")
