5b8a4dd8ccd5fbfed1dd0dc25e95b1243a2f4daa
AliSDspring2022 <trpkan7@gmail.com>
2022-11-02 22:41:01 -0400

    created promotion handling (only queen for now)

--- game.py
+++ game.py
@@ -11,8 +11,10 @@ DIMENSION = 8
 SQ_SIZE = HEIGHT // DIMENSION
 FPS = 15 # max fps
 IMAGES = {}
-
+promotion_pos = ['a8','b8','c8','d8','e8','f8','g8','h8','a1','b1','c1','d1','e1','f1','g1','h1']
+pawn = ['P','p']
 board = chess.Board()
+board_state = br.board_init(board)
 
 
 def update_board(board):
@@ -21,13 +23,29 @@ def update_board(board):
     return returned
 
 
-def check_legal(move):
+def check_legal(move , from_pos , to_pos, y, z):
+    check_promo = board_state[z][y]
+    print(check_promo)
+    if check_promo == 'p':
+        if to_pos in promotion_pos:
+            print("we in")
+            to_pos = to_pos + "q"
+            move = from_pos + to_pos
+            print(move)
+            print(list(board.legal_moves))
+    elif check_promo == 'P':
+        if to_pos in promotion_pos:
+            to_pos = to_pos + "q"
+            move = from_pos + to_pos
+            print(move)
+            print(list(board.legal_moves))
     if chess.Move.from_uci(move) in board.legal_moves:
+        print(list(board.legal_moves))
         board.push_san(move)
         return 1
     else:
-        return 0
-
+        print(list(board.legal_moves))
+        print("not legal")
 def random_move(board):
     move_holder = list(board.legal_moves)
     move_returned = random.choice(move_holder)
@@ -54,11 +72,11 @@ def game_status(board):
 
 if __name__ == "__main__":
     p.init()
+    x = 0
     screen = p.display.set_mode((WIDTH, HEIGHT))
     clock = p.time.Clock()
     screen.fill(p.Color("White"))
     br.load_images()
-    board_state = br.board_init(board)
     running = True
     square_selected = () # last click of the player
     playerInput = []
@@ -74,13 +92,23 @@ if __name__ == "__main__":
                         square_selected = () # reset square
                         playerInput = [] # reset input
                     else:
+                        x = x + 1
+                        print(x)
                         square_selected = (row,col)
                         playerInput.append(square_selected)
                     if len(playerInput) == 2:
                         from_pos = br.board_position(playerInput[0][0],playerInput[0][1])
                         to_pos = br.board_position(playerInput[1][0],playerInput[1][1])
+                        x = 0
+                        z = playerInput[0][0]
+                        y = playerInput[0][1]
                         whole_pos = from_pos + to_pos
-                        check_legal(whole_pos)
+                        print(whole_pos)
+                        check_legal(whole_pos , from_pos, to_pos, y,z)
+                        if (board.turn == chess.WHITE):
+                            print("white")
+                        else:
+                            print("black")
                         square_selected = ()  # reset square
                         playerInput = [] # reset input
 
@@ -90,6 +118,4 @@ if __name__ == "__main__":
         clock.tick(FPS)
         p.display.flip()
         game_status(board)
-        move = "a2a3"
-        check_legal(move)
         board_state = br.board_init(board)
