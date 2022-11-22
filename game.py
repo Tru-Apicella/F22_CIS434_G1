cde44a71ca325367bb5d4fa4ae75228fc55b0c0b
wbadria <2804851@vikes.csuohio.edu>
2022-11-21 23:51:11 -0500

    menu UI

--- game.py
+++ game.py
@@ -6,14 +6,16 @@ import board as br
 import pygame as p
 import AI
 
+
 WIDTH = 512
 HEIGHT = 512
 DIMENSION = 8
 SQ_SIZE = HEIGHT // DIMENSION
-FPS = 15 # max fps
+FPS = 15  # max fps
 IMAGES = {}
-promotion_pos = ['a8','b8','c8','d8','e8','f8','g8','h8','a1','b1','c1','d1','e1','f1','g1','h1']
-pawn = ['P','p']
+promotion_pos = ['a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8',
+                 'h8', 'a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1']
+pawn = ['P', 'p']
 board = chess.Board()
 board_state = br.board_init(board)
 
@@ -24,10 +26,10 @@ def update_board(board):
     return returned
 
 
-def check_legal(move , from_pos , to_pos, y, z):
+def check_legal(move, from_pos, to_pos, y, z):
     check_promo = board_state[z][y]
     print(check_promo)
-    print(y,z)
+    print(y, z)
     if check_promo == 'p':
         if to_pos in promotion_pos:
             print("we in")
@@ -48,10 +50,13 @@ def check_legal(move , from_pos , to_pos, y, z):
     else:
         print(list(board.legal_moves))
         print("not legal")
+
+
 def random_move(board):
     move_holder = list(board.legal_moves)
     move_returned = random.choice(move_holder)
     return move_returned
+
 
 def game_status(board):
     if board.is_checkmate():
@@ -72,6 +77,7 @@ def game_status(board):
         msg = "draw: claim"
         print(msg)
 
+
 def main():
     p.init()
     x = 0
@@ -80,56 +86,57 @@ def main():
     screen.fill(p.Color("White"))
     br.load_images()
     running = True
-    square_selected = () # last click of the player
+    square_selected = ()  # last click of the player
     playerInput = []
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
             elif e.type == p.MOUSEBUTTONDOWN:
-                    location = p.mouse.get_pos()
-                    col = location[0]//SQ_SIZE
-                    row = location[1]//SQ_SIZE
-                    if square_selected == (row,col):
-                        square_selected = () # reset square
-                        playerInput = [] # reset input
+                location = p.mouse.get_pos()
+                col = location[0]//SQ_SIZE
+                row = location[1]//SQ_SIZE
+                if square_selected == (row, col):
+                    square_selected = ()  # reset square
+                    playerInput = []  # reset input
+                else:
+                    x = x + 1
+                    print(x)
+                    square_selected = (row, col)
+                    playerInput.append(square_selected)
+                if len(playerInput) == 2:
+                    from_pos = br.board_position(
+                        playerInput[0][0], playerInput[0][1])
+                    to_pos = br.board_position(
+                        playerInput[1][0], playerInput[1][1])
+                    x = 0
+                    z_from = playerInput[0][0]
+                    y_from = playerInput[0][1]
+                    z = playerInput[1][0]
+                    y = playerInput[1][1]
+                    check_pawn = board_state[z_from][y_from]
+                    print(check_pawn)
+                    if check_pawn == 'p':
+                        z = z_from
+                        y = y_from
+                    elif check_pawn == 'P':
+                        z = z_from
+                        y = y_from
+                    whole_pos = from_pos + to_pos
+                    print(whole_pos)
+                    check_legal(whole_pos, from_pos, to_pos, y, z)
+                    if (board.turn == chess.WHITE):
+                        print("white")
                     else:
-                        x = x + 1
-                        print(x)
-                        square_selected = (row,col)
-                        playerInput.append(square_selected)
-                    if len(playerInput) == 2:
-                        from_pos = br.board_position(playerInput[0][0],playerInput[0][1])
-                        to_pos = br.board_position(playerInput[1][0],playerInput[1][1])
-                        x = 0
-                        z_from = playerInput[0][0]
-                        y_from = playerInput[0][1]
-                        z = playerInput[1][0]
-                        y = playerInput[1][1]
-                        check_pawn  = board_state[z_from][y_from]
-                        print(check_pawn)
-                        if check_pawn == 'p':
-                            z = z_from
-                            y = y_from
-                        elif check_pawn == 'P':
-                            z = z_from
-                            y = y_from
-                        whole_pos = from_pos + to_pos
-                        print(whole_pos)
-                        check_legal(whole_pos , from_pos, to_pos, y,z)
-                        if (board.turn == chess.WHITE):
-                            print("white")
-                        else:
-                            print("black")
-                        square_selected = ()  # reset square
-                        playerInput = [] # reset input
+                        print("black")
+                    square_selected = ()  # reset square
+                    playerInput = []  # reset input
 
         board_state = br.board_init(board)
         br.draw_game_state(screen, board_state)
         clock.tick(FPS)
         p.display.flip()
         game_status(board)
-
 
 
 if __name__ == "__main__":
--- start_menu.py
+++ start_menu.py
@@ -4,10 +4,13 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 screen.fill((0, 180, 210))
 
+# Upload the background image
+background = pygame.image.load("background.png")
+
 
 class Button:
     """
-    This class will create Start button and Quit button.
+    This class will create the menue buttons.
     """
 
     def __init__(self, color, x, y, width, height, text=""):
@@ -28,7 +31,7 @@ class Button:
                          (self.x, self.y, self.width, self.height), 0)
 
         if self.text != '':
-            font = pygame.font.SysFont('Verdana', 20)
+            font = pygame.font.SysFont('gabriola', 35)
             text = font.render(self.text, 1, (0, 0, 0))
             window.blit(text, (self.x + (self.width / 2 - text.get_width() / 2),
                         self.y + (self.height / 2 - text.get_height() / 2)))
@@ -43,6 +46,8 @@ class Button:
 
 def redraw_menu_window():
     screen.fill((153, 141, 141))
+    # Draw the background image on the screen
+    screen.blit(background, (0, 0))
     vs_player_btn.draw(screen, (0, 0, 0))
     vs_AI_btn.draw(screen, (0, 0, 0))
     quit_button.draw(screen, (0, 0, 0))
@@ -52,9 +57,10 @@ def redraw_game_window():
     screen.fill((0, 0, 0))
 
 
-vs_player_btn = Button((38, 74, 56), 100, 200, 250, 100, "Player Vs. Player")
-vs_AI_btn = Button((38, 74, 56), 450, 200, 250, 100, "Player Vs. Computer")
-quit_button = Button((138, 55, 55), 450, 200, 250, 100, "Quit")
+vs_player_btn = Button((255, 239, 213), 100, 200,
+                       250, 100, "Player Vs. Player")
+vs_AI_btn = Button((255, 239, 213), 450, 200, 250, 100, "Player Vs. Computer")
+quit_button = Button((255, 239, 213), 275, 400, 250, 100, "Quit")
 
 game_state = "menu"
 run = True
@@ -79,18 +85,26 @@ while run:
                     print("clicked the button")
                     game_state = "game"
                     g.main()  # call main class
-                if quit_button.is_over(mouse_position):
+                if vs_AI_btn.is_over(mouse_position):
                     print("clicked the 2button")
+                    game_state = "game"
+                    g.main()  # call main class
+                if quit_button.is_over(mouse_position):
+                    print("clicked the 3button")
                     run = False
                     pygame.quit()
                     quit()
 
             if event.type == pygame.MOUSEMOTION:
                 if vs_player_btn.is_over(mouse_position):
-                    vs_player_btn.color = (105, 105, 105)
+                    vs_player_btn.color = (108, 85, 79, 255)
                 else:
-                    vs_player_btn.color = (38, 74, 56)
+                    vs_player_btn.color = (255, 239, 213)
+                if vs_AI_btn.is_over(mouse_position):
+                    vs_AI_btn.color = (108, 85, 79, 255)
+                else:
+                    vs_AI_btn.color = (255, 239, 213)
                 if quit_button.is_over(mouse_position):
-                    quit_button.color = (105, 105, 105)
-                else:
                     quit_button.color = (138, 55, 55)
+                else:
+                    quit_button.color = (255, 239, 213)
