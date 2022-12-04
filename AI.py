46f6b75421342d353f55019e239254fa9bb7e25c
Tru-Apicella <truapicella@gmail.com>
2022-12-04 01:38:48 -0500

    removed junk, ai optmztn, bug fixes, currnt ai bug

--- AI.py
+++ AI.py
@@ -1,10 +1,7 @@
 import game
 import random
-import random
 
 MAX, MIN = 10000, -10000
-CNODE = 0
-
 
 PBias = [
         [0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
@@ -116,7 +113,6 @@ class Piece:
         self.x = []
         self.y = []
         self.eval = []
-        self.boards = []
         self.nodes = []
 
 
@@ -139,42 +135,30 @@ def convertPos(i):
         return 7
 
 
-# randomizes all the moves the legal move the ai can make
-def getRandomMove(self):
-    legalMoves = list(game.check_legal)
-    randomMove = random.choice(legalMoves)
-    return randomMove
-
-# createMove creates nodes for every move that contain piece current position and next position
-
-
 def createMove(board, board_state, det):
     x = 0
     moves = []
     while (True):
         try:
-            moves.append(str(list(board.legal_moves)[x]))
+            moves.append(str(list(board.pseudo_legal_moves)[x]))
             x += 1
         except:
             break
 
+    if moves == []:
+        print("break")
     origPos, origPos2, newPos, newPos2, posActual, nPosActual = [], [], [], [], [], []
     tmp = 0
     for i in moves:
         tmp = convertPos(i[:1])
         origPos.append(tmp)
-    for i in moves:
         tmp = convertPos(i[1:2])
         origPos2.append(tmp)
-    for i in moves:
         tmp = convertPos(i[2:3])
         newPos.append(tmp)
-    for i in moves:
         tmp = convertPos(i[3:])
         newPos2.append(tmp)
-    for i in moves:
         posActual.append(i[:2])
-    for i in moves:
         nPosActual.append(i[2:])
     p1 = [0]*len(moves)
     i = 0
@@ -188,11 +172,6 @@ def createMove(board, board_state, det):
                     (p1[x].nextPos).append(nPosActual[i])
                     (p1[x].x).append(newPos2[i])
                     (p1[x].y).append(newPos[i])
-                    nboard = game.check_legal(
-                        (p1[x].pos+p1[x].nextPos[-1]), p1[x].pos, p1[x].nextPos[-1], p1[x].y[-1], p1[x].x[-1], board, board_state)
-                    nboard_state = game.br.board_init(nboard)
-                    (p1[x].boards).append(nboard_state)
-                    board.pop()
                     count += 1
         if count == 0 or i == 0:
             p1[createdNode] = Piece()
@@ -201,90 +180,79 @@ def createMove(board, board_state, det):
             p1[createdNode].piece = board_state[origPos2[i]][origPos[i]]
             (p1[createdNode].x).append(newPos2[i])
             (p1[createdNode].y).append(newPos[i])
-            try:
-                nboard = game.check_legal((p1[createdNode].pos+p1[createdNode].nextPos[0]), p1[createdNode].pos,
-                                          p1[createdNode].nextPos[0], p1[createdNode].y[0], p1[createdNode].x[0], board, board_state)
-                nboard_state = game.br.board_init(nboard)
-                (p1[createdNode].boards).append(nboard_state)
-                board.pop()
-            except:
-                print("lazy bug fix")
             createdNode += 1
         i += 1
     p1 = list(filter(None, p1))
     if det == 0:
         random.shuffle(p1)
-        global CNODE
-        h = CNODE + createdNode
-        CNODE = h
         return p1, createdNode
     elif det == 1:
         return p1
 
 
-def createTree(board, board_state, depth, iters, alpha, beta):
-    iters += 1
+def createTree(board, board_state, depth, alpha, beta):
     p1, createdNode = createMove(board, board_state, 0)
     p1 = createEval(p1, createdNode, board_state)
-    if depth == 2:
-        if board.turn == False:
-            lowest = p1[0].eval
-            return p1, depth, lowest
-        elif board.turn == False:
-            highest = p1[0].eval
-            return p1, depth, highest
+    if depth == 3:
+        try:
+            if board.turn == False:
+                lowest = p1[0].eval
+                return p1, lowest
+            elif board.turn == False:
+                highest = p1[0].eval
+                return p1, highest
+        except:
+            print("break")
     if board.turn == True:
         best = MIN
         for x in range(createdNode):
             for y in range(len(p1[x].x)):
-                if depth < 2:
-                    nboard = game.check_legal((p1[x].pos+p1[x].nextPos[y]), p1[x].pos, p1[x].nextPos[y], p1[x].y[y], p1[x].x[y], board, board_state)
+                if depth < 3:
+                    if board_state == None:
+                        print("break")
+                    nboard,test = game.check_legal((p1[x].pos+p1[x].nextPos[y]), p1[x].pos, p1[x].nextPos[y], p1[x].y[y], p1[x].x[y], board, board_state,1)
                     nboard_state = game.br.board_init(nboard)
-                    depth += 1
-                    tmp, depth, val = (createTree(nboard, nboard_state, depth, iters, alpha, beta))
-                    depth -= 1
+                    tmp, val = (createTree(nboard, nboard_state, (depth+1), alpha, beta))
                     board.pop()
-                    (p1[x].nodes).append(tmp)
-                    try:
-                        best = max(best, val)
-                    except:
-                        val.append(best)
-                        best = max(val)
-                    alpha = max(alpha,best)
+                    if test == 0:
+                        (p1[x].nodes).append(tmp)
+                        try:
+                            best = max(best, val)
+                        except:
+                            val.append(best)
+                            best = max(val)
+                        alpha = max(alpha,best)
 
-                    if beta <= alpha:
-                        return p1,depth,best
-        if iters != 1:
-            iters -= 1
-            return p1, depth, best
+                        if beta <= alpha and depth != 0:
+                            return p1,best
+        if depth != 0:
+            return p1, best
 
     else:
         best = MAX
         for x in range(createdNode):
             for y in range(len(p1[x].x)):
-                if depth < 2:
-                    nboard = game.check_legal((p1[x].pos+p1[x].nextPos[y]), p1[x].pos, p1[x].nextPos[y], p1[x].y[y], p1[x].x[y], board, board_state)
+                if depth < 3:
+                    if board_state == None:
+                        print("break")
+                    nboard,test = game.check_legal((p1[x].pos+p1[x].nextPos[y]), p1[x].pos, p1[x].nextPos[y], p1[x].y[y], p1[x].x[y], board, board_state,1)
                     nboard_state = game.br.board_init(nboard)
-                    depth += 1
-                    tmp, depth, val = (createTree(nboard, nboard_state, depth, iters, alpha, beta))
-                    depth -= 1
+                    tmp, val = (createTree(nboard, nboard_state, (depth+1), alpha, beta))
                     board.pop()
-                    (p1[x].nodes).append(tmp)
-                    try:
-                        val.append(best)
-                        best = min(val)
-                    except:
-                        best = min(best, val)
+                    if test == 0:
+                        (p1[x].nodes).append(tmp)
+                    
+                        try:
+                            val.append(best)
+                            best = min(val)
+                        except:
+                            best = min(best, val)
 
-                    beta = min(beta, best)
-
-                    if beta <= alpha:
-                        return p1,depth,best
-
-        if iters != 1:
-            iters -= 1
-            return p1, depth, best
-
+                        beta = min(beta, best)
+                        if beta <= alpha and depth != 0:
+                            return p1,best
+        if depth != 0:
+            return p1, best
     return p1
 
 
@@ -300,18 +268,25 @@ def otherSearchTree(p1):
         try:
             if p1[0].nodes:
                 for x in enumerate(p1):
+                    print("5")
                     for y in enumerate(x[1].nodes):
+                        print("6")
                         lowest, bestPos, bestNPos = otherSearchTree(y[1])
+                        print("7")
                         x[1].eval[y[0]] = lowest-x[1].eval[y[0]]
         except:
             print("This try except should fix the error i was having")
         lowest = 9999
         for x in p1:
+            print("8")
             if lowest > min(x.eval):
+                print("9")
                 lowest = min(x.eval)
                 bestPos = x.pos
                 for y in enumerate(x.eval):
+                    print("10")
                     if y[1] == lowest:
+                        print("11")
                         bestNPos = x.nextPos[y[0]]
         return lowest, bestPos, bestNPos
     elif p1[0].piece == 'P' or p1[0].piece == 'R' or p1[0].piece == 'N' or p1[0].piece == 'B' or p1[0].piece == 'Q' or p1[0].piece == 'K':
@@ -325,14 +300,17 @@ def otherSearchTree(p1):
             print("This try except should fix the error i was having")
         highest = -9999
         for x in p1:
+            print("1")
             if highest < max(x.eval):
+                print("2")
                 highest = max(x.eval)
                 bestPos = x.pos
                 for y in enumerate(x.eval):
+                    print("3")
                     if y[1] == highest:
                         bestNPos = x.nextPos[y[0]]
+                        print("4")
         return highest, bestPos, bestNPos
-
 
 # created tree of the best moves for ai & randomizes the best move so ai doesnt choice same thing everytime
 def getBestMove(self):
@@ -342,7 +320,9 @@ def getBestMove(self):
 
 
 def AIRunner(board, board_state):
-    p1 = createTree(board, board_state, 0, 0, MIN, MAX)
-    highest, bestPos, bestNPos = otherSearchTree(p1)
-    print(CNODE)
+    p1 = createTree(board, board_state, 0, MIN, MAX)
+    try:
+        highest, bestPos, bestNPos = otherSearchTree(p1)
+    except:
+        print ("bosnia fish")
     return bestPos, bestNPos
--- game.py
+++ game.py
@@ -5,7 +5,7 @@ import random
 import board as br
 import pygame as p
 import AI
-
+import os
 WIDTH = 512
 HEIGHT = 512
 DIMENSION = 8
@@ -19,7 +19,7 @@ board = chess.Board()
 board_state = br.board_init(board)
 
 
-def highlightPrev(screen, board_state, legal_move, square_selected, history):
+def highlightPrev(screen, history):
     if history != 0:
         s = p.Surface((SQ_SIZE, SQ_SIZE))
         s.set_alpha(50)  # transperancy value
@@ -63,7 +63,7 @@ def update_board(board):
     return returned
 
 
-def check_legal(move, from_pos, to_pos, y, z, board, board_state):
+def check_legal(move, from_pos, to_pos, y, z, board, board_state,permission):
     check_promo = board_state[z][y]
     print(check_promo)
     print(y, z)
@@ -85,19 +85,18 @@ def check_legal(move, from_pos, to_pos, y, z, board, board_state):
     if chess.Move.from_uci(move) in board.legal_moves:
         print(list(board.legal_moves))
         board.push_san(move)
-        return board
+        return board, 0
+    elif chess.Move.from_uci(move) in board.pseudo_legal_moves and permission == 1:
+        tmp = chess.Move.from_uci(move)
+        print(list(board.legal_moves))
+        board.push(tmp)
+        return board, 1
     else:
         print(list(board.legal_moves))
         print("not legal")
 
 
 # this function has no use currently, but it did help with debugging earlier
-
-
-def random_move(board):
-    move_holder = list(board.legal_moves)
-    move_returned = random.choice(move_holder)
-    return move_returned
 
 
 def game_status(board):
@@ -116,6 +115,8 @@ def game_status(board):
         textRect = text.get_rect()
         textRect.center = (256, 556)
         screen.blit(text, textRect)
+        p.display.flip()
+        os.system("pause")
 
     elif board.is_stalemate():
         msg = "draw: stalemate"
@@ -165,14 +166,6 @@ def main(type):
     clock = p.time.Clock()
     screen.fill(p.Color("White"))
 
-    # # hhhhhhhhhhhhhhhhh
-    # font = p.font.Font('freesansbold.ttf', 32)
-    # text = font.render('GeeksForGeeks', True, (0, 255, 0), (0, 0, 128))
-    # textRect = text.get_rect()
-    # textRect.center = (200, 550)
-    # screen.blit(text, textRect)
-    # # hhhhhhhhhhhhhhhhhhhhhh
-
     # load images once to avoid consuming resources
     br.load_images()
     running = True
@@ -220,30 +213,26 @@ def main(type):
                         if chess.Move.from_uci(whole_pos) in board.legal_moves:
                             history = playerInput
                         check_legal(whole_pos, from_pos, to_pos,
-                                    y, z, board, board_state)
+                                    y, z, board, board_state,0)
 
                         # print whose turn it is for better debugging and player experince
-                        if (board.turn == chess.WHITE):
+                        if board.turn == chess.WHITE and type == 0:
                             print("white")
-                            # hhhhhhhhhhhhhhhhh
                             font = p.font.Font('freesansbold.ttf', 52)
                             text = font.render(
                                 'Current Turn: White', True, (0, 0, 0), (255, 255, 255))
                             textRect = text.get_rect()
                             textRect.center = (256, 556)
                             screen.blit(text, textRect)
-                            # hhhhhhhhhhhhhhhhhhhhhh
                         else:
                             if type == 0:
                                 print("black")
-                                # hhhhhhhhhhhhhhhhh
                                 font = p.font.Font('freesansbold.ttf', 52)
                                 text = font.render(
                                     'Current Turn: Black', True, (0, 0, 0), (255, 255, 255))
                                 textRect = text.get_rect()
                                 textRect.center = (256, 556)
                                 screen.blit(text, textRect)
-                                # hhhhhhhhhhhhhhhhhhhhhh
                         square_selected = ()  # reset square
 
                         playerInput = []  # reset input
@@ -257,7 +246,15 @@ def main(type):
             if chess.Move.from_uci(pos + nextPos) in board.legal_moves:
                 history = input
             check_legal((pos + nextPos), pos, nextPos,
-                        np0, np1, board, board_state)
+                        np0, np1, board, board_state,0)
+            '''
+            kings=0
+            for x in board_state:
+                for y in x:
+                    if y == 'k' or y == 'K':
+                        kings +=1
+            if kings != 2:'''
+
         # loop for updating the game board and screen
         board_state = br.board_init(board)
         br.draw_game_state(screen, board_state, check_legal, square_selected)
@@ -265,7 +262,7 @@ def main(type):
         highlightSquares(screen, board_state, board.legal_moves, square_selected)
         p.display.flip()
         game_status(board)
-        highlightPrev(screen, board_state, board.legal_moves, square_selected, history)
+        highlightPrev(screen, history)
         p.display.flip()
         game_status(board)
 
